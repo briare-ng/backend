c59d4da2fba771f4ad8386fdfda7d6d2346384b8
Briare Nguyen <briare-nguyen@outlook.fr>
2024-06-20 19:36:24 +0200

    update url.js

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = "https://mymoviz-backend-eosin.vercel.app/";
+const BACKEND_URL = "https://backend-my-movies.vercel.app/";
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = "https://mymoviz-frontend-vert.vercel.app/";
+const FRONTEND_URL = "https://frontend-my-movies.vercel.app/";
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
